7e01472caf9c105fb7a9f2519f6fcf5ebfd28db2
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-07-29 19:12:48 +0530

    feat: created homepage

--- src/pages/homePage/homePage.jsx
+++ src/pages/homePage/homePage.jsx
@@ -1,3 +1,41 @@
+import { useNavigate } from "react-router-dom";
+import { categories } from "../../backend/categories";
+import { SideNav } from "../../components/sideNav/sideNav";
+import "../homePage/homePage.css";
+
 export const HomePage = () => {
-  return <div>This is a home page</div>;
+  const navigate = useNavigate();
+
+  const handleCategoryClick = (category) => {
+    navigate(`/listing/${category}`);
+  };
+
+  return (
+    <div className="homepage-main-container">
+      <div>
+        <SideNav />
+      </div>
+      <div className="homepage-container">
+        <div className="category-header">Categories</div>
+        <div className="categories-layout">
+          {categories.map((category) => (
+            <div
+              className="category-card"
+              onClick={() => handleCategoryClick(category.category)}
+              key={category.id}
+            >
+              <div>
+                <img
+                  src={category.thumbnail}
+                  alt="loading"
+                  className="category-thumbnail"
+                />
+              </div>
+              <div className="category-title">{category.category}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
