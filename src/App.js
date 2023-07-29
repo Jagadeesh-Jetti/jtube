e08d160ff01431f40088c83771dd05d36925a6a7
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-07-29 19:15:15 +0530

    update: updated files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { HomePage } from './pages/homePage/homePage';
+import { ListingPage } from './pages/listingPage/listingPage';
+import { VideoDetail } from './pages/videoDetail/videoDetail';
+import { WatchLater } from './pages/watchLater/watchLater';
 
 function App() {
   return (
     <div className="App">
-
-     
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/listing/:categorySelected" element={<ListingPage />} />
+        <Route path="/listing/:categorySelected/:videoSelected" element={<VideoDetail />} />
+        <Route path="/watchlater" element={<WatchLater />} />
+      </Routes>
     </div>
   );
 }
